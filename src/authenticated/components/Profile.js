130e82fee40ee37db2f9f782d269a595091830ed
raghubandaru <raghubandaru45@gmail.com>
2020-04-06 17:52:24 +0530

    Refactor Profile

--- src/authenticated/components/Profile.js
+++ src/authenticated/components/Profile.js
@@ -1,17 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
 
+import DialogUpload from './DialogUpload'
 import { StyledHeader, Main } from '../../shared/components'
-import Avatar from './Avatar'
+import { useUser } from '../../shared/context/User'
+import { Button } from '../../shared/elements'
+import UserInfo from './UserInfo'
 
-function Profile() {
+function Profile({ className }) {
+  const [avatarEdit, setAvatarEdit] = useState(false)
+
+  const { user } = useUser()
+
+  const close = () => setAvatarEdit(false)
+
   return (
     <>
       <StyledHeader title="Profile" quote="Access the information provided" />
       <Main>
-        <Avatar />
+        <div className={className}>
+          {user.avatar && (
+            <div className="image-content">
+              <img
+                src={`http://res.cloudinary.com/raghubandaru/image/upload/v${user.avatar}`}
+                alt="Avatar"
+              />
+            </div>
+          )}
+          <UserInfo />
+          <Button variant="secondary" onClick={() => setAvatarEdit(true)}>
+            {user.avatar ? 'Change Avatar' : 'Add Avatar'}
+          </Button>
+          <DialogUpload isOpen={avatarEdit} close={close} />
+        </div>
       </Main>
     </>
   )
 }
 
-export { Profile }
+const StyledProfile = styled(Profile)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+
+  .image-content {
+    width: 200px;
+    height: 200px;
+    margin-bottom: 2rem;
+    border-radius: 50%;
+
+    img {
+      width: 100%;
+      border-radius: 50%;
+    }
+  }
+`
+
+Profile.propTypes = {
+  className: PropTypes.string.isRequired
+}
+
+export { StyledProfile as Profile }
